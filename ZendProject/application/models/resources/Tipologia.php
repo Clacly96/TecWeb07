6df7e9be6bb477f0f29dade856ba9bc284449374
iaco96 <39337827+iaco96@users.noreply.github.com>
2018-05-20 15:13:43 +0200

    Aggiustamenti: tipologia->Tipologia, name->Nome

--- ZendProject/application/models/resources/Tipologia.php
+++ ZendProject/application/models/resources/Tipologia.php
@@ -2,7 +2,7 @@
 
 class Application_Resource_Tipologia extends Zend_Db_Table_Abstract
 {
-    protected $_name    = 'tipologia';
+    protected $_name    = 'Tipologia';
     protected $_primary  = 'Nome';
     protected $_rowClass = 'Application_Resource_Tipologia_Item';
     
@@ -13,7 +13,7 @@ class Application_Resource_Tipologia extends Zend_Db_Table_Abstract
 	
     public function estraiCategorie()// Estrae tutte le categorie Categorie ordinate per nome
     {
-		$select = $this->select()->order('name');
+		$select = $this->select()->order('Nome');
         return $this->fetchAll($select);
     }
  
