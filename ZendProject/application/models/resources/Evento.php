f4e5c7c807a2d2f1c8b4fc0704d3fac73161a359
iaco96 <39337827+iaco96@users.noreply.github.com>
2018-05-20 15:12:02 +0200

    Cambiato nome attributo evento in Evento

--- ZendProject/application/models/resources/Evento.php
+++ ZendProject/application/models/resources/Evento.php
@@ -2,7 +2,7 @@
 
 class Application_Resource_Evento extends Zend_Db_Table_Abstract
 {
-    protected $_name    = 'evento';
+    protected $_name    = 'Evento';
     protected $_primary  = 'Id';
     protected $_rowClass = 'Application_Resource_Evento_Item';
 
