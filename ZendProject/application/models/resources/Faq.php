dbd04dfc682da2196e0c76601bb878ea9c2baf76
dimaximo96 <dimaximo22@gmail.com>
2018-05-21 12:08:30 +0200

    Aggiunta Faq
    Aggiunto faq model e faq model resource, solo abbozzato

---
+++ ZendProject/application/models/resources/Faq.php
@@ -0,0 +1,35 @@
+<?php
+class Application_Resource_Faq extends Zend_Db_Table_Abstract{
+    protected $_name='faq';
+    protected $_primary='Id';
+    protected $_rowClass = 'Application_Resource_Faq_Item';
+    
+    	public function init()
+    {
+    }
+    public function estraiFaqPerId($Id)  //chiedere se le faq sono direttamente visibili o occorre cliccarci
+    {
+        return $this->find($id)->current();
+    }
+    
+    
+        public function estraiFaq($paged=null)
+    {
+         $select = $this->select()->order('Id ASC');
+         if (null !== $paged) {
+			$adapter = new Zend_Paginator_Adapter_DbTableSelect($select);   
+			$paginator = new Zend_Paginator($adapter);  
+			$paginator->setItemCountPerPage(7)
+		          	  ->setCurrentPageNumber((int) $paged); 
+			return $paginator;
+		}
+        return $this->fetchAll($select);
+    } 
+    }
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
