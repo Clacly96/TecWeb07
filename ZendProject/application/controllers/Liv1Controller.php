a6f24d779ed15a492e2fade3e946c6d3a08cd800
Roberto Broccoletti <roby.brokk.96@gmail.com>
2018-05-21 12:44:55 +0200

    Fatta l'action al controller

--- ZendProject/application/controllers/Liv1Controller.php
+++ ZendProject/application/controllers/Liv1Controller.php
@@ -2,12 +2,22 @@
 
 class Liv1Controller extends Zend_Controller_Action
 {
+    protected $_catalogModel;
     public function init()
     {
         $this->_helper->layout->setLayout('main');
+        $this->_catalogModel=new Application_Model_Catalogo();
+        
     }
     public function indexAction()
     {
+        $page=$this->_getParam('page',1);
+        $pagescont=$this->_getParam('pagescont',1);
+        $ultimieventi=$this->_catalogModel->estraiUltimiEventi($page);
+        $eventiscontati=$this->_catalogModel->ottieniEventiInSconto($pagescont);
+        $this->view->assign(array( 'ultimieventi' => $ultimieventi,
+                                    'eventiscontati' => $eventiscontati));
+        
     }
     public function vistastaticaAction()
     {
