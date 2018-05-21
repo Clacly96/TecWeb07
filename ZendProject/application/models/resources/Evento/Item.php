7f057202451659dafa65e5ea8e517580fafd3998
Roberto Broccoletti <roby.brokk.96@gmail.com>
2018-05-21 19:59:48 +0200

    modificata ottieniprezzoscontato in ottieniprezzo
    modificata per evitare di fare il controllo dove viene richiamata

--- ZendProject/application/models/resources/Evento/Item.php
+++ ZendProject/application/models/resources/Evento/Item.php
@@ -6,12 +6,15 @@ class Application_Resource_Evento_Item extends Zend_Db_Table_Row_Abstract
     {
     }
     
-    public function ottieniPrezzoScontato($evento)
+    public function ottieniPrezzo($evento)
     {
+        $data =Zend_Date::TIMESTAMP;
         $prezzo = $evento->Prezzo_Biglietto;
-        $percSconto=$evento->Sconto;
-        $sconto=$prezzo*$percSconto/100;
-        $prezzo=round($prezzo - $sconto, 2); //arrotonda a due cifre decimali
+        if($evento->Sconto>0 && $data>=$evento->Data_Inizio_Sconto){
+            $percSconto=$evento->Sconto;
+            $sconto=$prezzo*$percSconto/100;
+            $prezzo=round($prezzo - $sconto, 2); //arrotonda a due cifre decimali
+        }
         return $prezzo;
     }
     
