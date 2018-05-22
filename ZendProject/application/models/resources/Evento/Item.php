b0543d74820bee6ee9713a8ee4c0ca500ca6dd24
Claudio Spada <claudiospada@outlook.it>
2018-05-22 15:19:14 +0200

    Update Item.php

--- ZendProject/application/models/resources/Evento/Item.php
+++ ZendProject/application/models/resources/Evento/Item.php
@@ -6,19 +6,15 @@ class Application_Resource_Evento_Item extends Zend_Db_Table_Row_Abstract
     {
     }
     public function scontato(){
-        $data =Zend_Date::TIMESTAMP;
-        if($this->Sconto>0 && $data>=$this->Data_Inizio_Sconto){
-            return 1;
-        }
-        else {
-            return 0;
-        }
+        $datazend=new Zend_Date();
+        $data=$datazend->get(Zend_Date::W3C);
+        return ($this->Sconto>0 && $data>=$this->Data_Inizio_Sconto) ? true : false;
     }
 
     public function ottieniPrezzo()
     {
         $prezzo = $this->Prezzo_Biglietto;
-        if($this->scontato()==true){
+        if($this->scontato()){
             $percSconto=$this->Sconto;
             $sconto=$prezzo*$percSconto/100;
             $prezzo=round($prezzo - $sconto, 2); //arrotonda a due cifre decimali
