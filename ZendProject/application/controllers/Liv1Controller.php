4fe7cf8806bf2f6d0acc3d6f6db8c77878bead8a
Roberto Broccoletti <roby.brokk.96@gmail.com>
2018-05-22 19:32:27 +0200

    aggiunti file necessari per inserire form

--- ZendProject/application/controllers/Liv1Controller.php
+++ ZendProject/application/controllers/Liv1Controller.php
@@ -4,11 +4,13 @@ class Liv1Controller extends Zend_Controller_Action
 
 {
     protected $_catalogModel;
+    protected $_form;
     
     public function init()
     {
        $this->_helper->layout->setLayout('main');
         $this->_catalogModel = new Application_Model_Catalogo();
+        $this->view->filtroForm = $this->getFiltroForm();
     }
     public function indexAction()
     {
@@ -23,7 +25,16 @@ class Liv1Controller extends Zend_Controller_Action
        
         $IdEv=$this->_getParam('evento',null);
         $paged = $this->_getParam('page', 1);
-        if(!is_null($IdEv)){
+        $tiporic=$this->_getParam('tiporic',null);
+        if(!is_null($tiporic)){
+            if($tiporic=='filtro'){
+                
+                
+            } else if($tiporic=='ricerca'){
+                //paolo metti il codice relativo alla ricerca qua
+            }
+                else $this->_helper->redirector('catalogo','liv1');   
+        }else if(!is_null($IdEv)){
             $eventi=$this->_catalogModel->estraiEventoPerId($IdEv);
         }
         else { $eventi=$this->_catalogModel->estraiEventi($paged);}
@@ -39,6 +50,18 @@ class Liv1Controller extends Zend_Controller_Action
         
         
         
+    }
+    private function getFiltroForm()
+    {
+        $urlHelper = $this->_helper->getHelper('url');
+        $this->_form = new Application_Form_Liv1_Filtri_Filtro();
+        $this->_form->setAction($urlHelper->url(array(
+                'controller' => 'liv1',
+                'action' => 'catalogo',
+                                'tiporic' => 'filtro'),
+                'default'
+                ));
+        return $this->_form;
     }
 }
 
---
+++ ZendProject/application/forms/Liv1/Filtri/Filtro.php
@@ -0,0 +1,84 @@
+<?php
+class Application_Form_Liv1_Filtri_Filtro extends App_Form_Abstract
+{
+        protected $_catalogModel;
+        protected $_userModel;
+        
+        public function init(){
+            $this->_catalogModel= new Application_Model_Catalogo();
+            $this->_userModel= new Application_Model_Utenza(); 
+            $this->setMethod('post');
+            $this->setName('filtroCatalogo');
+            $this->setAction('');
+            $this->setAttrib('enctype', 'application/x-www-form-urlencoded');
+            
+            $categorie = array();
+		$cats = $this->_catalogModel->estraiCategorie();
+		foreach ($cats as $cat) {
+			$categorie[$cat -> Nome] = $cat->Nome;
+		}
+		$this->addElement('select', 'Nome', array(
+                        'label' => 'Categoria',
+                        'required' => false,
+			'multiOptions' => $categorie,
+                        'value' => null
+		));
+                
+                $organizzazioni = array();
+		$orgs = $this->_userModel->getOrg();
+		foreach ($orgs as $org) {
+			$organizzazioni[$org -> Username] = $org->Username;
+		}
+		$this->addElement('select', 'Username', array(
+                        'label' => 'Organizzazioni',
+                        'required' => false,
+			'multiOptions' => $organizzazioni,
+                        'value' => null
+		));
+                
+                $luoghi = array();
+		$luogs = $this->_catalogModel->estraiLuoghi();
+		foreach ($luogs as $luogo) {
+			$luoghi[$luogo->Luogo] = $luogo->Luogo;
+		}
+		$this->addElement('select', 'Luogo', array(
+                        'label' => 'Luoghi',
+                        'required' => false,
+			'multiOptions' => $luoghi,
+                        'value' => null
+		));
+                
+                $this->addElement('date', 'Data_Ora', array(
+                        'label' => 'Data',
+                        'required' => false,
+                        'value' => null
+		));
+                
+                
+
+             /*   $date = new Element_Date('appointment-date');
+                $date->setLabel('Appointment Date');
+                $date->setAttributes([
+                    'min'  => '2012-01-01',
+                    'max'  => '2020-01-01',
+                    'step' => '1', // days; default step interval is 1 day
+                ]);
+                $date->setOptions([
+                    'format' => 'Y-m-d',
+                ]);
+
+                
+                $this->add($date);*/
+
+                $this->addElement('submit', 'filtra', array(
+                        'label' => 'Filtra Eventi',
+		));
+                
+                
+
+            
+            
+        }
+    
+    
+}
