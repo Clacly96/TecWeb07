33daf16cadf3937a523fe330cbf1f2fb6604ee61
Claudio Spada <claudiospada@outlook.it>
2018-05-23 11:12:04 +0200

    Modifiche minori

--- ZendProject/application/views/helpers/AnteprimaEvento.php
+++ ZendProject/application/views/helpers/AnteprimaEvento.php
@@ -2,7 +2,7 @@
 class Zend_View_Helper_AnteprimaEvento extends Zend_View_Helper_HtmlElement
 {
 	private $_tipo;
-
+	
 	public function AnteprimaEvento($evento, $tipo = 'anteprima', $link=null,$path=null)
 	{
             $tag='hai sbagliato parametro tipo, metti o anteprima o singolo o scontato';
@@ -12,30 +12,6 @@ class Zend_View_Helper_AnteprimaEvento extends Zend_View_Helper_HtmlElement
                         . '<div class="descev">Descrizione:<br>'. $evento->estraiDescrBreve() .'...</div>'
                         . '<div class="przev"> Prezzo: '. $this->view->prezzoEventi($evento,true) .'</div>'
                         . '</div><img src="'. $this->view->baseUrl('/images/locandine/' . $evento->Locandina) .'">'
-<<<<<<< HEAD
-                        . '<div class="titoloev">'.$evento->Nome.'</div></a></li>';
-
-                if($tipo=='scontato') $tag= '<li class="sconto_li">'
-                        . '<a href="'.  $this->view->url(array('controller' => 'liv1' , 'action' => 'catalogo', 'evento' => $evento->Id),'default',true) .'">'
-                        . '<div class="prezzi_sconto">'
-                        . '<div class="titoloev">'.  $evento->Nome  .'</div> '
-                        . '<div class="old_price">Prezzo:'. $evento->Prezzo_Biglietto .'euro</div>'
-                        . '<div class="new_price">Prezzo:'. $this->view->prezzoEventi($evento) .'</div>'
-                        . '<div class="sconto">Sconto:'.$evento->Sconto .'%</div>'
-                        . '</div>'
-                        . '<img src="'. $this->view->baseUrl('/images/locandine/' . $evento->Locandina).'"></a></li>';
-
-                if($tipo=='singolo') {
-
-                    $tag= '<div id="SingoloEvento"> '
-                        . '<h1>'.$evento->Nome.'</h1>'
-                        . '<img src="'.$this->view->baseUrl('/images/locandine/' . $evento->Locandina).'"><div id="info_biglietti"> ';
-
-                      if($evento->scontato()) $tag=$tag.'<div class="old_price">Vecchio prezzo:'.$evento->Prezzo_Biglietto.'</div>'
-                              . '<div><b> Sconto: </b>'.$evento->Sconto.'%</div>' ;
-
-                    $tag=$tag .'<div><b> Prezzo: </b>'.$this->view->prezzoEventi($evento).'</div> '
-=======
                         . '<div class="titoloev">'.$evento->Nome.'</div></a></li>';}
                 
                 if($tipo=='scontato') {$tag= '<li class="sconto_li">'
@@ -58,7 +34,6 @@ class Zend_View_Helper_AnteprimaEvento extends Zend_View_Helper_HtmlElement
                               . '<div><b> Sconto: </b>'.$evento->Sconto.'%</div>' ;
                       
                     $tag=$tag .'<div><b> Prezzo: </b>'.$this->view->prezzoEventi($evento,true).'</div> '
->>>>>>> Model
                         . '<div><b> Biglietti rimanenti: </b>'.$evento->Biglietti_Rimanenti.'</div></div>'
                         . '<div><b> Luogo: </b> '.$evento->Luogo.'</div> '
                         . '<div><b> Data: </b> '.$evento->Data_Ora.'</div> <br><br> '
