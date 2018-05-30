750f321efdb12fe28ed44abc6907499a302d56d6
iaco96 <39337827+iaco96@users.noreply.github.com>
2018-05-30 21:53:14 +0200

    aggiunta funzione per update dati utente

--- ZendProject/application/models/resources/Utente.php
+++ ZendProject/application/models/resources/Utente.php
@@ -69,4 +69,20 @@ class Application_Resource_Utente extends Zend_Db_Table_Abstract {
                      );
         $insert=$this->insert($dati);
     }
+	
+    public function updateUtente ($info) {
+        
+        $residenza=$info['Citta'].'-'.$info['Via'].'-'.$info['Civico'];
+        $where['Username = ?'] = $info['Username'];
+        $dati = array( 'Username' => $info['Username'],
+		    'Password' => $info['Password'],
+		    'Nome' => $info['Nome'],
+		    'Cognome' => $info['Cognome'],
+		    'Email' => $info['Email'],
+                    'Residenza' => $residenza,
+		    'Ruolo' => 'utente',
+                    'Telefono' => $info['Telefono']
+                     );
+        $update=$this->update($dati, $where);
+    }
 }
